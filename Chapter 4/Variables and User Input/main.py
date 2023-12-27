9eaea0c197e719df365976c6c768ddf1c28c0b5c
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2023-12-26 18:03:33 -0800

    Create main.py

---
+++ Chapter 4/Variables and User Input/main.py
@@ -0,0 +1,12 @@
+#Name: Priya Harry
+#Date: 28 October 2023
+#Assignment Name: 4.2 Exercise B
+
+#Taking the users name
+name = input("Please enter your name: ")
+print("Welcome " + name + ".")
+#Taking the users age (I changed phrasing)
+age = int(input("How old are you currently?"))
+#adding 10 years to age to calc. future age
+age = age + 10
+print(name + ", you will be " + str(age) + " years old in 10 years.")
