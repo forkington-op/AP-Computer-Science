06074f3255ac3f6b91c38155f8f0f2d094f71bea
CaptainSapphire <122335357+CaptainSapphire@users.noreply.github.com>
2023-12-26 17:57:23 -0800

    Create main.py

---
+++ Chapter 4/What went Wrong? - 4.1 Excercise/main.py
@@ -0,0 +1,28 @@
+# This code has 5 syntax errors! Find them and correct them.
+# When you have found and corrected all five, run the program
+# and verify that it works. You can change the initial score
+# to test different outcomes of the program.
+
+# Create a variable called score, setting it to 10
+score = 10
+
+# If the score is zero or less
+if (score <= 0):
+  # Output to the user that they lose
+  print("You Lose!")
+else:
+  # Output - You Win! - and the score to the user
+  print("You Win!")
+  print("Your score is: ")
+  print(score)
+
+  # Wish the user good luck next time (If they win AND if they lose)
+  print("Good luck next time.")
+
+
+#Issues that I fixed: 
+# 1. Line 10 "scor" --> "score"
+# 2. Line 12 added ""
+# 3. Line 13 added :
+# 4. Line 16 "prnit" --> "print"
+# 5. added ()
